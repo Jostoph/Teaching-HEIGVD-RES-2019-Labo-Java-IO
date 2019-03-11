70e9dc75799b0ac8393bfded6a2997112a890334
Christoph Rueff <christoph.rueff@gmail.com>
2019-03-11 14:43:13 +0100

    Implement FileNumberingFilterWriter

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/FileNumberingFilterWriter.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/FileNumberingFilterWriter.java
@@ -1,5 +1,7 @@
 package ch.heigvd.res.labio.impl.filters;
 
+import ch.heigvd.res.labio.impl.Utils;
+
 import java.io.FilterWriter;
 import java.io.IOException;
 import java.io.Writer;
@@ -19,23 +21,49 @@ public class FileNumberingFilterWriter extends FilterWriter {
 
   private static final Logger LOG = Logger.getLogger(FileNumberingFilterWriter.class.getName());
 
+  private int lineNum;
+  private boolean rBreak;
+
   public FileNumberingFilterWriter(Writer out) {
     super(out);
+    this.lineNum = 1;
+    this.rBreak = false;
+
   }
 
   @Override
   public void write(String str, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    write(str.toCharArray(), off, len);
   }
 
   @Override
   public void write(char[] cbuf, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    for(int i = off; i < off + len; i++) {
+      write(cbuf[i]);
+    }
   }
 
   @Override
   public void write(int c) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+    if(lineNum == 1) {
+      super.write(lineNum + "\t",0, (int) (Math.log10(lineNum) + 2));
+      lineNum++;
+    }
 
+    if(c == '\n') {
+      rBreak = false;
+      super.write("\n" + lineNum + "\t",0, (int) (Math.log10(lineNum) + 3));
+      lineNum++;
+    } else if(c == '\r') {
+      rBreak = true;
+      super.write(c);
+    } else {
+      if(rBreak) {
+        rBreak = false;
+        super.write(lineNum + "\t",0, (int) (Math.log10(lineNum) + 2));
+        lineNum++;
+      }
+      super.write(c);
+    }
+  }
 }
